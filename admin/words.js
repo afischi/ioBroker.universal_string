95f7c730ce3081b66ff963d8f9fbdadaaaa33dd7
Afischi <urksle@gmx.de>
2020-04-05 20:40:20 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"universal_string adapter settings": {
+		"en": "Adapter settings for universal_string",
+		"de": "Adaptereinstellungen für universal_string",
+		"ru": "Настройки адаптера для universal_string",
+		"pt": "Configurações do adaptador para universal_string",
+		"nl": "Adapterinstellingen voor universal_string",
+		"fr": "Paramètres d'adaptateur pour universal_string",
+		"it": "Impostazioni dell'adattatore per universal_string",
+		"es": "Ajustes del adaptador para universal_string",
+		"pl": "Ustawienia adaptera dla universal_string",
+		"zh-cn": "universal_string的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
